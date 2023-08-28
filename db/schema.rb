3d74711e44b67abef7f7df4ddc7a6e62607972c5
Benjamin Pachevie <b.pachevie@gmail.com>
2023-08-28 15:25:57 +0200

    game places model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_131818) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_132312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "game_places", force: :cascade do |t|
+    t.boolean "found"
+    t.bigint "place_id", null: false
+    t.bigint "game_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_game_places_on_game_id"
+    t.index ["place_id"], name: "index_game_places_on_place_id"
+  end
 
   create_table "games", force: :cascade do |t|
     t.integer "radius"
@@ -47,5 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_131818) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "game_places", "games"
+  add_foreign_key "game_places", "places"
   add_foreign_key "games", "users"
 end
---
+++ test/models/game_place_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class GamePlaceTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
