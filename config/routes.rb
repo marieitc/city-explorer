c26abc350bd670b610a8b8b45d77489917ad4c40
Benjamin Pachevie <b.pachevie@gmail.com>
2023-08-28 16:46:32 +0200

    finishing partially routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :games, only: %i[new create show] do
+    get '/lobby', to: 'games#lobby'
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+# new -> create -> show (lobby) -> show (game started)
