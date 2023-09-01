60a76d8b1ea9f8a00be34d8eb8b88323716421a7
Benjamin Pachevie <b.pachevie@gmail.com>
2023-09-01 15:24:29 +0200

    personalized avatar

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :user
-  has_many :participations
+  has_many :participations, dependent: :destroy
   has_many :game_places, dependent: :destroy
   has_many :places, through: :game_places
 
