b80ed4217dc32f0be348ade9e5836cdd2a4ea774
Benjamin Pachevie <b.pachevie@gmail.com>
2023-09-07 10:55:31 +0200

    slider for input radius

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -10,30 +10,12 @@ export default class extends Controller {
     const swiper = new Swiper(this.swiperContainerTarget, {
       effect: "cards",
       grabCursor: true,
-      loop: true
     });
-
-    swiper.on('click', (event) => {
-      const currentCard = event.slides.find(slide => slide.classList.contains("swiper-slide-active"))
-      const placeId = currentCard.dataset.placeId
-
-      const clickedCardEvent = new CustomEvent("swiper:card:clicked", {
-        detail: {
-          placeId: placeId
-        },
-      })
-      document.dispatchEvent(clickedCardEvent)
-    })
 
     swiper.on('slideChange', (event) => {
       this.validateTarget.classList.add("d-none")
       this.cards.forEach(card => {
         card.querySelector("img").classList.remove("selected-img")
-        if (this.map.getPaintProperty(`area-${evt.params.placeId}`, "circle-color", "#393939") === "#393939") {
-          this.map.setPaintProperty(`area-${evt.params.placeId}`, "circle-color", "#5cbfcc");
-        } else if (this.map.getPaintProperty(`area-${evt.params.placeId}`, "circle-color", "#5cbfcc") === "#5cbfcc" ) {
-          this.map.setPaintProperty(`area-${evt.params.placeId}`, "circle-color", "#393939");
-        }
       });
     });
   }
