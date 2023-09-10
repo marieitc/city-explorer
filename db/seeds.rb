f6216774b67a76d7bd5868238cb8e237471c230f
Marie Intoci <130375758+marieitc@users.noreply.github.com>
2023-09-10 14:10:09 +0200

    add new places to find north paris

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,20 +16,20 @@ puts 'Creating Places'
 
 # create le wagon
 
-# Place.create!(address: '16 Rue du Repos, 75020 Paris, France', name: 'Père Lachaise', url_image: 'https://upload.wikimedia.org/wikipedia/commons/b/b0/Main_gate_of_the_P%C3%A8re-Lachaise_Cemetery%2C_Paris_13_June_2014.jpg',
-#               points: 20)
+Place.create!(address: '16 Rue du Repos, 75020 Paris, France', name: 'Père Lachaise', url_image: 'https://upload.wikimedia.org/wikipedia/commons/b/b0/Main_gate_of_the_P%C3%A8re-Lachaise_Cemetery%2C_Paris_13_June_2014.jpg',
+              points: 20)
 Place.create!(address: 'Place de la République, 75010 Paris, France', name: 'Statue de la République', url_image: 'https://paris1900.lartnouveau.com/paris10/place_de_la_republique/statues/1stat_rep01.jpg',
               points: 10)
-Place.create!(address: '12-16 Villa Gaudelet, 75011 Paris, France', name: 'Le Wagon', url_image: 'https://studysmarter-mediafiles.s3.amazonaws.com/media/uploads/froala_editor/images/fa7583826ca3-Paris_Campus_3__3_.png',
-              points: 50)
+# Place.create!(address: '12-16 Villa Gaudelet, 75011 Paris, France', name: 'Le Wagon', url_image: 'https://studysmarter-mediafiles.s3.amazonaws.com/media/uploads/froala_editor/images/fa7583826ca3-Paris_Campus_3__3_.png',
+#               points: 50)
 Place.create!(address: 'Place Georges Pompidou, 75004 Paris', name: 'Centre Pompidou', url_image: 'https://uploads.lebonbon.fr/source/2017/paris/site/ines/centre-pompidou-.jpg',
               points: 10)
-# Place.create!(address: '47 Rue des Couronnes, 75020 Paris, France', name: 'Parc de Belleville', url_image: 'https://i.pinimg.com/originals/52/40/db/5240dbfbaf8efe30a46df6b2c60250df.jpg',
-#               points: 20)
-# Place.create!(address: 'Square Saint-Ambroise, 75011 Paris', name: 'Eglise Saint-Ambroise', url_image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/5f/55/04/eglise-saint-ambroise.jpg?w=1200&h=-1&s=1',
-#               points: 30)
-# Place.create!(address: '50 Boulevard Voltaire, 75011 Paris', name: 'Bataclan', url_image: 'https://cdn.sortiraparis.com/images/80/1665/182946-le-bataclan-de-paris-devrait-rouvrir-fin-2016.jpg',
-#               points: 20)
+Place.create!(address: '47 Rue des Couronnes, 75020 Paris, France', name: 'Parc de Belleville', url_image: 'https://i.pinimg.com/originals/52/40/db/5240dbfbaf8efe30a46df6b2c60250df.jpg',
+              points: 20)
+Place.create!(address: 'Square Saint-Ambroise, 75011 Paris', name: 'Eglise Saint-Ambroise', url_image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/5f/55/04/eglise-saint-ambroise.jpg?w=1200&h=-1&s=1',
+              points: 30)
+Place.create!(address: '50 Boulevard Voltaire, 75011 Paris', name: 'Bataclan', url_image: 'https://cdn.sortiraparis.com/images/80/1665/182946-le-bataclan-de-paris-devrait-rouvrir-fin-2016.jpg',
+              points: 20)
 Place.create!(address: '4 rue Eugène Spuller, 75003 Paris', name: 'Le Carreau du Temple', url_image: 'https://parisjetaime.com/data/layout_image/fr-FR/Carreau-du-temple-Exterieur-630x405-C-Fernando-Javier-Urquijo.jpg',
               points: 10)
 Place.create!(address: '60 rue Réaumur, 75003 Paris', name: 'Musée des Arts et métiers', url_image: 'https://tzmedia.b-cdn.net/media/images/fr/place/420/42d4850cf4d261562535a764b1c3ba93.jpg?1587732836',
@@ -56,20 +56,75 @@ Place.create!(address: 'Rue Crémieux, 75012 Paris', name: 'Rue Crémieux', url_
               points: 30)
 Place.create!(address: "3 Pass. de l'Ancre 75003 Paris", name: "Passage de l'Ancre", url_image: 'https://1.bp.blogspot.com/-4Ep1lJjygSg/WY8ej-1zmNI/AAAAAAAAuo0/zT3xJngiRPc6WNFDy6ZEJ4UfbPrrOufzwCLcBGAs/s1600/passage-de-l-ancre-paris-3-7.JPG',
               points: 40)
-poupees = Place.create!(address: '5 Vila Gaudelet 75011 Paris', name: 'Poupées Vaudoux',
-                        points: 40)
+Place.create!(address: "Pl. Charles de Gaulle 75008 Paris", name: "Arc de Triomphe", url_image: 'https://static.actu.fr/uploads/2022/07/arc-triomphe-paris-secrets-anecdote-soldat-inconnu-napoleon-histoire-patrimoine.jpg',
+              points: 20)
+Place.create!(address: "Pl. de l'Opéra 75009 Paris", name: "Palais Garnier", url_image: 'https://uploads.lebonbon.fr/source/2022/november/2038012/couverture-ope-ra-national-de-paris_2_1200.jpg',
+              points: 20)
+Place.create!(address: "5 Avenue Anatole France 75007 Paris", name: "Tour Eiffel", url_image: 'https://cdn.sortiraparis.com/images/80/83517/756379-visuel-paris-tour-eiffel.jpg',
+              points: 20)
+Place.create!(address: "Pl. de la Madeleine 75008 Paris", name: "Eglise de la Madeleine", url_image: 'https://www.pariszigzag.fr/wp-content/uploads/2019/06/eglise-madeleine-paris-zigzag-e1560246076792.png',
+              points: 40)
+Place.create!(address: "35 Bd de Courcelles 75008 Paris", name: "Parc Monceau", url_image: 'https://www.unjourdeplusaparis.com/wp-content/uploads/2022/01/colonnade-naumachie-monceau.jpg',
+              points: 20)
+Place.create!(address: "8 Av. César Caire 75008 Paris", name: "Eglise Saint-Augustin", url_image: 'https://www.fondationavenirpatrimoineparis.fr/wp-content/uploads/2018/11/IMG_0666.jpg',
+              points: 40)
+Place.create!(address: "Cour Carrée du Louvre 75001 Paris", name: "Musée du Louvre", url_image: 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Paris_-_Palais_du_Louvre_-_PA00085992_-_1343.jpg',
+              points: 30)
+Place.create!(address: "Pont Alexandre III 75008 Paris", name: "Pont Alexandre III", url_image: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Golden_statue_on_Pont_Alexandre_III_1.jpg',
+              points: 20)
+Place.create!(address: "Jardin du Luxembourg 75006 Paris", name: "Jardin du Luxembourg", url_image: 'https://upload.wikimedia.org/wikipedia/commons/8/82/La_fontaine_M%C3%A9dicis%2C_Jardin_du_Luxembourg%2C_Paris_2013.jpg',
+              points: 50)
+Place.create!(address: "33 Av. du Maine 75015 Paris", name: "Tour Montparnasse", url_image: 'https://static.actu.fr/uploads/2020/09/tour-montparnasse-paris-3764787-1920.jpg',
+              points: 20)
+Place.create!(address: "Place de la concorde 75008 Paris", name: "Fontaine de la concorde", url_image: 'https://media.vanityfair.fr/photos/60d34b7eb8495767dcde92f0/master/w_2580%2Cc_limit/giuseppe_masci_agf_uig_via_getty_images1_jpg_999.jpg',
+              points: 50)
+Place.create!(address: "35 Rue du Chevalier de la Barre, 75018 Paris", name: "Basilique du Sacré-Cœur", url_image: 'https://www.transilien.com/sites/transilien/files/styles/manual_crop/public/2020-03/Sacr%C3%A9%20Coeur%20vignette.jpg?itok=pdtBMNQS',
+              points: 20)
+Place.create!(address: "Pl. des Abbesses 75018 Paris", name: "Le Mur des Je t'aime", url_image: 'https://cdn.sortiraparis.com/images/80/94880/869701-paris-decouvrez-la-belle-histoire-du-mur-des-je-t-aime-a-montmartre.jpg',
+              points: 30)
+Place.create!(address: "15 rue Crémieux 75012 Paris", name: "Rue Crémieux", url_image: 'https://media.vanityfair.fr/photos/60d34b80db141fd0eb5b5427/master/w_2580%2Cc_limit/roger_rozencwajg___photononstop_jpg_9312.jpg',
+              points: 20)
+Place.create!(address: "2 Rue de Montpensier 75001 Paris", name: "Colonnes de Buren", url_image: 'https://media.vanityfair.fr/photos/60d34b848759f470f20a41f0/master/w_2580%2Cc_limit/gettyimages_1048362104_jpg_7524.jpg',
+              points: 40)
+Place.create!(address: "7 Rue de l'Abreuvoir 75018 Paris", name: "Rue de l'Abreuvoir", url_image: 'https://images.prismic.io/utopix-next-website/MTQ2MjkzNjYtMGE0Yy00ODQzLTkzOTEtOGVjYTQ1ZDM0ODAy_187f5d38-6a53-4bac-8f02-f8a6f04b89ea_ruedel27abreuvoir.jpg?ixlib=js-3.8.0&w=3840&auto=format&fit=max',
+              points: 20)
+Place.create!(address: "Pl. d'Estienne d'Orves 75009 Paris", name: "Eglise de la Sainte Trinité", url_image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/%C3%89glise_de_la_Sainte-Trinit%C3%A9_de_Paris_Face.JPG',
+              points: 30)
+Place.create!(address: "25 Rue Chappe 75018 Paris", name: "Arène de Montmartre", url_image: 'https://www.montmartre-addict.com/wp-content/uploads/2022/07/festival-solidaire-arenes-montmartre.jpeg',
+              points: 40)
+Place.create!(address: "8bis Rue Choron 75009 Paris", name: "Notre-Dame de Lorette", url_image: 'https://www.liberation.fr/resizer/I8IbFh88ci5gbQw_qX_ZFR7V0bw=/arc-photo-liberation/eu-central-1-prod/public/M4YG5RGXNNGNVKGBZLOLIMROF4.jpg',
+              points: 30)
+Place.create!(address: "Pl. Vendôme 75001 Paris", name: "Colonne Vendôme", url_image: 'https://upload.wikimedia.org/wikipedia/commons/e/ef/Statue_Napol%C3%A9on_Colonne_Vend%C3%B4me_Paris_2.jpg',
+              points: 20)
+Place.create!(address: "2 Imp. Saint-Eustache 75001 Paris", name: "Église Saint-Eustache", url_image: 'https://www.narthex.fr/oeuvres-et-lieux/images-oeuvres-et-lieux-2019/eustache-14-flanc-sud-690.jpg/image',
+              points: 10)
+Place.create!(address: "Pl. Joachim du Bellay 75001 Paris", name: "Fontaine des Innocents", url_image: 'https://upload.wikimedia.org/wikipedia/commons/f/fd/Fontaine_des_Innocents%2C_2011.JPG',
+              points: 30)
+Place.create!(address: "Square de la Tour Saint-Jacques 75004 Paris", name: "Tour Saint-Jacques", url_image: 'https://parisjetaime.com/data/layout_image/fr-FR/Tour-Saint-Jacques-630x405-C-OTCP-Marc-Bertrand-I-161-52.jpg',
+              points: 20)
+Place.create!(address: "Pl. de l'Hôtel de Ville 75004 Paris", name: "Hôtel de Ville", url_image: 'https://upload.wikimedia.org/wikipedia/commons/d/d7/H%C3%B4tel_de_Ville_de_Paris_d%C3%A9tail_fa%C3%A7ade.jpg',
+              points: 30)
+Place.create!(address: "1 Rue de l'Oratoire 75001 Paris", name: "Arcades rivoli", url_image: 'https://ch.schreder.com/sites/default/files/2019-07/urban-residential_streets_fr_schreder_retrofit_rivoli_4.jpg',
+              points: 40)
+Place.create!(address: "Pl. Jean-Paul II 75004 Paris", name: "Notre Dame de Paris", url_image: 'https://upload.wikimedia.org/wikipedia/commons/4/43/Cath%C3%A9drale_Notre-Dame_de_Paris_-_05.jpg',
+              points: 20)
 
-poupees.photo.attach(
-  io: File.open(Rails.root.join('app', 'assets', 'images', 'poupeevaudou.png')),
-  filename: 'poupeevaudou.png', content_type: 'image/png'
-)
 
-# Place.create!(address: '12 Rue Lechevin, 75011 Paris', name: 'Ménagerie de Verre', url_image: 'https://paris-promeneurs.com/wp-content/uploads/2018/03/menagerie-verre-500.jpg',
-#   points: 40)
-# Place.create!(address: '72 Ave Parmentier, 75011 Paris', name: 'Biocoop Parmentier', url_image: 'https://images.happycow.net/venues/1024/17/76/hcmp177601_693082.jpeg',
-#   points: 30)
-# Place.create!(address: '64 Ave Parmentier, 75011 Paris', name: 'Regus Parmentier', url_image: 'https://groupeterrot.com/wp-content/uploads/2020/01/TERROT_AV-PARMENTIER_1118_JPG-HD20181130_01-1024x683.jpg.webp',
-#   points: 20)
+
+
+
+
+
+# Le raincy Test
+Place.create!(address: "83 Av. de la Résistance 93340 Le Raincy", name: "Église Notre-Dame de Consolation", url_image: 'https://upload.wikimedia.org/wikipedia/fr/thumb/5/5b/%C3%89glise_Notre-Dame_du_Raincy_-_M%C3%A9rim%C3%A9e_PA00079948_-_1.jpeg/800px-%C3%89glise_Notre-Dame_du_Raincy_-_M%C3%A9rim%C3%A9e_PA00079948_-_1.jpeg',
+              points: 20)
+Place.create!(address: "121 Av. de la Résistance 93340 Le Raincy", name: "Mairie du Raincy", url_image: 'https://upload.wikimedia.org/wikipedia/fr/thumb/5/5b/%C3%89glise_Notre-Dame_du_Raincy_-_M%C3%A9rim%C3%A9e_PA00079948_-_1.jpeg/800px-%C3%89glise_Notre-Dame_du_Raincy_-_M%C3%A9rim%C3%A9e_PA00079948_-_1.jpeg',
+              points: 20)
+Place.create!(address: "26 Chem. des Bourdons 93220 Gagny", name: "Parc Gustave Courbet", url_image: 'https://lh5.googleusercontent.com/p/AF1QipNkkm_PhDfiY9nyB34yf56o5VIG3bIwbrRUkZnd',
+                points: 20)
+
+
+
 
 puts 'Places created'
 
